5b9144a66012fde57109b26b1f8bd36d6b317a33
devbean <ohb4199@naver.com>
2024-11-07 23:20:29 +0900

    chore : 마우스 CSS 추가 - hover 시 yellow 나올 수 있도록 개선 - over 시 pointer 나올 수 있도록 개선

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <div className='w-full h-16 bg-cyan-400 flex justify-center items-center'>
       <div className='flex justify-center items-center mt-1 h-500'>
-        <div className='text-lg mr-5 text-white'>홈</div>
-        <div className='text-lg mr-5 text-white'>커뮤니티</div>
-        <div className='text-lg mr-5 text-white'>동아리</div>
-        <div className='text-lg mr-5 text-white'>스터디</div>
-        <div className='text-lg mr-5 text-white'>모집 요청</div>
+        <div className='text-lg mr-5 text-white hover:text-yellow-400 cursor-pointer'>홈</div>
+        <div className='text-lg mr-5 text-white hover:text-yellow-400 cursor-pointer'>커뮤니티</div>
+        <div className='text-lg mr-5 text-white hover:text-yellow-400 cursor-pointer'>동아리</div>
+        <div className='text-lg mr-5 text-white hover:text-yellow-400 cursor-pointer'>스터디</div>
+        <div className='text-lg mr-5 text-white hover:text-yellow-400 cursor-pointer'>모집 요청</div>
       </div>
     </div>
   )

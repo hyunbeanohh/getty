bbd726fae18d2a996686d4a1e07b4432dbfac7f7
devbean <ohb4199@naver.com>
2024-11-11 23:56:01 +0900

    Feat : 현재 모집 중인 동아리 마크업(1)

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>
+    <div className='w-full h-[75px] bg-white'>
       <div className='w-full h-10 flex justify-center items-center'>
         <input className='w-[500px] h-10 rounded-md mt-10 outline-none bg-gray-100 rounded-2xl pl-4' type="text" placeholder='관심있는 동아리를 입력해주세요 :)' />
       </div>
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../components/Header"
 import SearchBar from "../../components/SearchBar"
+import NowApplyClub from "../../components/NowApplyClub"
+
 function MainPage() {
     return (
-        <div>
+        <div className="w-full h-screen bg-gray-100">
             <Header />
             <SearchBar />
+            <NowApplyClub />
         </div>
     );
 }

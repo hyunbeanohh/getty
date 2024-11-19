4b5dd36fdc3305af4e5bbb8fa90228f9e9ceef0a
devbean <ohb4199@naver.com>
2024-11-19 23:00:06 +0900

    Feat : 인기 동아리 순위 컴포넌트 마크업(2) - 목업 데이터 카드 형태로 표현 - 재사용 할 수 있는 컴포넌트 추가

--- src/components/ClubRanking.tsx
+++ src/components/ClubRanking.tsx
@@ -7,14 +7,30 @@ const ClubRanking = () => {
     { id: 1, name: '동아리1', rank: 1 , image: guideImage},
     { id: 2, name: '동아리2', rank: 2 , image: guideImage},
     { id: 3, name: '동아리3', rank: 3 , image: guideImage},
+    { id: 4, name: '동아리4', rank: 4 , image: guideImage},
+    { id: 5, name: '동아리5', rank: 5 , image: guideImage}
+      
   ];
   
   const Card = ({ club, img }: { club: any, img: any }) => {
     return (
-      <div>
-        <img src={img} alt={club.name} />
-        <span>{club.name} - {club.rank}</span>
-      </div>
+      <article>
+        <div className='flex w-[250px] h-[350px] mb-10'>
+          <a className='w-full h-full' href='javascript:;' onClick={()=>{
+            {/* 동아리 상세 페이지로 이동 */}
+          }}>
+            <span className='block w-full h-full'>
+              <img src={img} alt={club.name} className='w-full h-full object-fit'/>
+            </span>
+          </a>
+          <div>
+            {/* 동아리 이름 */}
+            {/* 동아리 순위 */}
+            {/* 동아리 남은 모집 날짜 */}
+          </div>
+        </div>
+        
+      </article>
     );
   };
 
@@ -29,12 +45,15 @@ const ClubRanking = () => {
           />
         </div>
 
-        <div className='flex items-center'>
+        <div>
           <span className='relative text-lg block left-10 mt-10 mb-5 font-semibold font-pretendard'>인기 동아리 순위</span>
-          <div className='flex flex-col items-center col-12 col-sm-4'>
+          <div className='flex flex-wrap justify-center gap-10'>
             {/* 임시 동아리 데이터 연동 */}
             {clubData.map((club) => (
-              <Card key={club.id} club={club} img={club.image}/>
+              <Card key={club.id}
+                    club={club} 
+                    img={club.image}
+              />
             ))}
           </div>
         </div>        

2569a1669fab0dccc60e5793068813e869892d70
devbean <ohb4199@naver.com>
2025-05-11 23:48:53 +0900

    refactor : PopularClub 컴포넌트 삭제 및 AllClubCardSection에서 관련 코드 정리

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -4,15 +4,13 @@ import SearchBar from "../../components/SearchBar"
 import AllClub from "../../components/AllClub"
 import AllClubCardSection from "../../components/AllClubCardSection"
 import HireITInfo from "../../components/hireITinfo"
-import PopularClub from "../../components/PopularClub"
+import PopularClub from "../../components/club/PopularClub"
 function MainPage() {
     return (
         <div className="w-full h-full bg-gray-100">
             <Header />
-            <SearchBar />
             <PopularClub />
             <AllClub />
-            <HireITInfo/>
         </div>
     );
 }

2e75f8a637ea9852e844a25b3da421ce43c821ad
devbean <ohb4199@naver.com>
2025-06-22 22:19:36 +0900

    style : 비활성 툴팁 위치 조정

--- src/components/club/ClubFilter.tsx
+++ src/components/club/ClubFilter.tsx
@@ -73,13 +73,13 @@ const ClubFilter = ({ onFilterChange, onSearchChange }: ClubFilterProps) => {
 
             {/* 고급 필터 토글 */}
             <div 
-              className="relative"
+              className="relative left-[50px]"
               onMouseEnter={() => setShowTooltip(true)}
               onMouseLeave={() => setShowTooltip(false)}
             >
               <button
                   disabled
-                  className="flex items-center gap-2 px-4 py-2 rounded-xl transition-all duration-200 relative left-[50px] bg-gray-100 text-gray-400 cursor-not-allowed opacity-50"
+                  className="flex items-center gap-2 px-4 py-2 rounded-xl transition-all duration-200  bg-gray-100 text-gray-400 cursor-not-allowed opacity-50"
                 >
                   <Filter className="w-5 h-5" />
                   필터
@@ -87,9 +87,9 @@ const ClubFilter = ({ onFilterChange, onSearchChange }: ClubFilterProps) => {
               
               {/* 커스텀 툴팁 */}
               {showTooltip && (
-                <div className="absolute top-full left-[90px] transform -translate-x-1/2 mt-2 bg-gray-800 text-white text-xs px-2 py-1 rounded z-10 whitespace-nowrap">
+                <div className="absolute top-full left-[55px] transform -translate-x-1/2 mt-2 bg-gray-800 text-white text-xs px-2 py-1 rounded z-10 whitespace-nowrap">
                   아직 준비중인 기능이에요!
-                  <div className="absolute bottom-full left-[90px] transform -translate-x-1/2 border-4 border-transparent border-b-gray-800"></div>
+                  <div className="absolute bottom-full left-[55px] transform -translate-x-1/2 border-4 border-transparent border-b-gray-800"></div>
                 </div>
               )}
             </div>

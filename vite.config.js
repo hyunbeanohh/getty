bb1101775530f6e91fa8aca1b991a81b7a4edd1e
devbean <ohb4199@naver.com>
2025-06-24 00:37:47 +0900

    chore: Vite 설정 변경 및 이미지 파일 이름 수정 - Vite 설정에 이미지 파일 처리 규칙 추가 - popularClubData.ts에서 DND, YAPP, MashUP 이미지 파일 이름 대문자로 수정

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     }
   },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        }
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {

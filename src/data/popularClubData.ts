04a2fa27275f9608a571a57e2df7fbf54eabc785
Oh Hyun bin <ohb4199@naver.com>
2025-06-24 00:39:01 +0900

    Revert "chore: Vite 설정 변경 및 이미지 파일 이름 수정"

--- src/data/popularClubData.ts
+++ src/data/popularClubData.ts
@@ -1,9 +1,9 @@
 import { Club } from '@/types/types';
 import Depromeet from '@/assets/images/Deproment.png';
-import DND from '@/assets/images/Dnd.png';
-import YAPP from '@/assets/images/Yapp.png';
+import DND from '@/assets/images/DND.png';
+import YAPP from '@/assets/images/YAPP.png';
 import DDD from '@/assets/images/DDD.png';
-import MashUP from '@/assets/images/MashUP.png';
+import MashUP from '@/assets/images/mashup.png';
 
 export const CLUBS: Club[] = [
   { id: 'depromeet', image: Depromeet, target: "https://www.depromeet.com", status: "OFF" },

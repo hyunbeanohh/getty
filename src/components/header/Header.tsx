6d55df27af2182282be69761b8fc34f2057a1127
devbean <ohb4199@naver.com>
2025-07-15 01:08:21 +0900

    style : 배경색 수정 , 헤더 영역 색상 변경 및 위치 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,10 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <div className='w-full h-16 bg-[#F1F7FF] flex justify-center items-center border-b border-gray-200 font-pretendard'>
+    <div className='w-full h-16 bg-white flex justify-between items-center border-b border-gray-200 font-pretendard px-8'>
+      <div className='flex-1'></div>
       <div className='flex justify-center items-center mt-1 h-500'>
-        <div className='text-lg mr-5 text-black hover:text-yellow-400 cursor-pointer'
+        <div className='text-lg mr-5 text-black cursor-pointer rounded-lg p-[10px] hover:bg-gray-100 transition-colors'
           onClick={() => {
             navigate('/');
           }}
@@ -13,15 +14,25 @@ const Header = () => {
           홈
         </div>
         <a 
-          className='text-lg mr-5 text-black hover:text-yellow-400 cursor-pointer'
+          className='text-lg mr-5 text-black cursor-pointer rounded-lg p-[10px] hover:bg-gray-100 transition-colors'
           href='https://news.hada.io/'
           rel='noopener noreferrer'
           target='_blank'
         >
             IT 뉴스
         </a>
+        <a 
+          className='text-lg mr-5 text-black cursor-pointer rounded-lg p-[10px] hover:bg-gray-100 transition-colors'
+          onClick={() => {
+            navigate('/techBlog');
+          }}
+        >
+            기술 블로그
+        </a>
+      </div>
+      <div className='flex-1 flex justify-end'>
         <div 
-          className='text-lg mr-5 text-black hover:text-yellow-400 cursor-pointer'
+          className='text-lg text-white cursor-pointer rounded-lg bg-[#3182f6] p-[10px] hover:bg-[#1b64da] transition-colors'
           onClick={() => {
             navigate('/request');
           }}
